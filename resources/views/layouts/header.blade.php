b3954255f9d1c196af6c051ef3125a72c876564a
Roman Sorin <988244@students.mentorschools.org>
2019-02-13 22:14:04 -0500

    More header development (should probably start using branches again)

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,33 +2,68 @@
 <!-- TODO: find the icons i want -->
 
 <div class="nav-wrapper">
-    <nav class="navbar justify-content-center" id="header">
+    <nav class="navbar justify-content-center header">
         <div class="container-fluid">
-            <button class="btn mr-auto header-menu-toggle"><span class="oi oi-menu" id="header-menu-open"></span>
-            </button> <!-- TODO: turn this into a different button with an 'is-active' class -->
+            <button class="btn mr-auto header-menu-toggle"><span class="oi oi-menu"></span>
+            </button> <!-- TODO: turn this into a different button with an 'is-active' class; turns into an x -->
             <a class="navbar-brand" id="header-brand" href="/">Text</a>
-            <button class="btn ml-auto"><span class="oi oi-magnifying-glass" id="search-display-toggle"></span></button>
+            <button class="btn ml-auto search-display-toggle"><span class="oi oi-magnifying-glass"></span></button>
+            <!-- also turns into an x -->
 
         </div>
     </nav>
     <nav id="menu">
         <ul class="nav flex-column">
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="/academics">Academics</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="/campus">Campus Life</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                <a class="nav-link" href="/the-arts">The Arts</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/resources">Resources</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/about">About</a>
             </li>
         </ul>
+    </nav>
+    <div id="search-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <form id="search-form" role="search" action="#">
+                        <input name="search" class="search-input" placeholder="Type to search" autocomplete="off"
+                               type="text">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="base">
+    <nav class="navbar justify-content-center header">
+        <div class="container-fluid">
+            <button class="btn mr-auto header-menu-toggle"><span class="oi oi-menu"></span>
+            </button> <!-- TODO: turn this into a different button with an 'is-active' class -->
+            <a class="navbar-brand" id="header-brand" href="/">Text</a>
+            <button class="btn ml-auto search-display-toggle"><span class="oi oi-magnifying-glass"></span></button>
+        </div>
     </nav>
 </div>
 
 <!-- TODO: using vue instead? -->
 <script>
     $('.header-menu-toggle').on('click', function () {
+        $('.nav-wrapper').toggleClass('is-open');
         $('#menu').toggle();
+    });
+    $('.search-display-toggle').on('click', function () {
+        $('.nav-wrapper').toggleClass('is-open');
+        $('#search-wrapper').toggle();
     });
 </script>
