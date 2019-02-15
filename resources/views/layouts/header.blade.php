fc2d539b6cbc5143eb38a539bbc831e96e222550
Roman Sorin <988244@students.mentorschools.org>
2019-02-15 18:25:58 -0500

    Header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,55 +2,46 @@
 <!-- TODO: find the icons i want -->
 
 <div class="nav-wrapper">
-    <nav class="navbar justify-content-center header">
-        <div class="container-fluid">
-            <button class="btn mr-auto header-menu-toggle"><span class="oi oi-menu"></span>
-            </button> <!-- TODO: turn this into a different button with an 'is-active' class; turns into an x -->
-            <a class="navbar-brand" id="header-brand" href="/">Text</a>
-            <button class="btn ml-auto search-display-toggle"><span class="oi oi-magnifying-glass"></span></button>
-            <!-- also turns into an x -->
-
+    <nav id="menu">
+        <div class="nav flex-column" id="menu-box">
+            <a class="nav-link" href="/academics">Academics</a>
+            <a class="nav-link" href="/campus">Campus Life</a>
+            <a class="nav-link" href="/the-arts">The Arts</a>
+            <a class="nav-link" href="/resources">Resources</a>
+            <a class="nav-link" href="/about">About</a>
         </div>
     </nav>
-    <nav id="menu">
-        <ul class="nav flex-column">
-            <li class="nav-item">
-                <a class="nav-link" href="/academics">Academics</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/campus">Campus Life</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/the-arts">The Arts</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/resources">Resources</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
-            </li>
-        </ul>
-    </nav>
-    <div id="search-wrapper">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <form id="search-form" role="search" action="#">
-                        <input name="search" class="search-input" placeholder="Type to search" autocomplete="off"
-                               type="text">
-                    </form>
-                </div>
+</div>
+
+<div id="search-wrapper">
+    <div class="container center">
+        <div class="row">
+            <div class="col-lg-12">
+                <form id="search-form" role="search" action="#">
+                    <input name="search" class="search-input" placeholder="Type to search" autocomplete="off"
+                           type="text">
+                </form>
             </div>
         </div>
     </div>
+    <!--<div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+
+            </div>
+        </div>
+    </div>-->
 </div>
 
 <div class="base">
     <nav class="navbar justify-content-center header">
         <div class="container-fluid">
-            <button class="btn mr-auto header-menu-toggle"><span class="oi oi-menu"></span>
-            </button> <!-- TODO: turn this into a different button with an 'is-active' class -->
-            <a class="navbar-brand" id="header-brand" href="/">Text</a>
+            <button class="hamburger hamburger--collapse header-menu-toggle mr-auto" type="button">
+                <span class="hamburger-box">
+                    <span class="hamburger-inner"></span>
+                </span>
+            </button>
+            <a class="navbar-brand" id="header-brand" href="/">Mentor</a>
             <button class="btn ml-auto search-display-toggle"><span class="oi oi-magnifying-glass"></span></button>
         </div>
     </nav>
@@ -58,12 +49,20 @@
 
 <!-- TODO: using vue instead? -->
 <script>
-    $('.header-menu-toggle').on('click', function () {
-        $('.nav-wrapper').toggleClass('is-open');
-        $('#menu').toggle();
+    const menu_toggle = document.querySelector('.header-menu-toggle');
+    const search_toggle = document.querySelector('.search-display-toggle');
+    const nav = document.querySelector('.nav-wrapper');
+    const menu = document.getElementById('menu');
+    const search = document.getElementById('search-wrapper');
+
+    menu_toggle.addEventListener('click', e => {
+        menu_toggle.classList.toggle('is-active');
+        nav.classList.toggle('is-open');
+        menu.classList.toggle('active');
     });
-    $('.search-display-toggle').on('click', function () {
-        $('.nav-wrapper').toggleClass('is-open');
-        $('#search-wrapper').toggle();
+    search_toggle.addEventListener('click', e => {
+        nav.classList.toggle('is-open');
+        search.style.display === '' ? search.style.display = 'block' : '';
+        search.classList.toggle('active');
     });
 </script>
