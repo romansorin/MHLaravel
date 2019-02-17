2de0132f3063c6c8fa327930ac5ce82f692bca5b
Roman Sorin <988244@students.mentorschools.org>
2019-02-16 21:19:14 -0500

    SVG icons, media queries header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -11,18 +11,18 @@
             <a class="nav-link" href="/about">About</a>
         </div>
     </nav>
-    <div class="menu-footer">
+    <!--<div class="menu-footer container">
         <div class="flex-row justify-content-start" id="menu-footer-row">
-            <a>Hello</a>
-            <a>Hello</a>
-            <a>Hello</a>
-            <a>Hello</a>
+            <a>Some</a>
+            <a>Filler</a>
+            <a>Text</a>
+            <a>Footer</a>
         </div>
-    </div>
+    </div>-->
 </div>
 
 <div id="search-wrapper">
-    <div class="close-search"><button class="btn btn-light">Close</button></div>
+    <div class="close-search"><button class="btn btn-light"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/><path d="M0 0h24v24H0z" fill="none"/></svg></button></div>
     <div class="container search-container">
         <div class="row">
             <div class="col-lg-12">
@@ -51,7 +51,7 @@
                 </span>
             </button>
             <a class="navbar-brand" id="header-brand" href="/">Mentor</a>
-            <button class="btn ml-auto search-display-toggle"><span class="oi oi-magnifying-glass"></span></button>
+            <button class="btn ml-auto search-display-toggle"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg></button>
         </div>
     </nav>
 </div>
