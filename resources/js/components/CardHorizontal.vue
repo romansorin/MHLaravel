459e555fa7cd57d1cb949f1bbc2ab04fc62a4a10
Roman Sorin <988244@students.mentorschools.org>
2019-02-24 15:39:57 -0500

    Cards & main content (no logic or Laravel integration yet)

---
+++ resources/js/components/CardHorizontal.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="col-12">
+        <article class="card-horizontal d-none d-lg-block" id="one">
+            <div class="container">
+                <div class="row card-horizontal-meta">
+                        <p class="card-horizontal-category">{{ data[0].category }}</p>
+                        <h3 class="card-horizontal-title">{{ data[0].title }}</h3>
+                </div>
+                <div class="row card-horizontal-view">
+                    <button type="button" class="btn btn-light card-horizontal-view-btn">View</button>
+                </div>
+            </div>
+        </article>
+    </div>
+</template>
+
+<script>
+    let data = [
+        {category: "VueJS", title: "This is a title, where I am trying to learn VueJS."}
+    ];
+
+    export default {
+        name: "CardHorizontal",
+        data () {
+            return {data}
+        }
+    }
+</script>
